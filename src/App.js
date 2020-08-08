711cf1d753a38276b237c6c9d7c914c5d75c533c
hark17 <harjeevs17@gmail.com>
2020-08-09 01:33:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import "weather-icons/css/weather-icons.css";
 import "bootstrap/dist/css/bootstrap.css";
