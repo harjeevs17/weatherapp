e8578a942d8ffc7878dc1ccb5ba44162fa14baee
hark17 <harjeevs17@gmail.com>
2020-08-09 01:13:30 +0530

    Updates

---
+++ precache-manifest.f622df70f4b59e160db38e9cdddd4f76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca00050311de1ed48edb9351a6043fc7",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "42d862b5ae22d1053718",
+    "url": "/weatherapp/static/css/2.540866eb.chunk.css"
+  },
+  {
+    "revision": "577f79caeb49e9854025",
+    "url": "/weatherapp/static/css/main.a3848f81.chunk.css"
+  },
+  {
+    "revision": "42d862b5ae22d1053718",
+    "url": "/weatherapp/static/js/2.943d4d51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weatherapp/static/js/2.943d4d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577f79caeb49e9854025",
+    "url": "/weatherapp/static/js/main.f9158965.chunk.js"
+  },
+  {
+    "revision": "86e9643be231c9f93fb5",
+    "url": "/weatherapp/static/js/runtime-main.f512d807.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weatherapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weatherapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weatherapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weatherapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weatherapp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
